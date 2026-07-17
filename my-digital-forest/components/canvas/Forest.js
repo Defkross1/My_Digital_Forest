64bdf1195afd95db3595a3327c1986874e28a4f4
Defkross1 <andresanhueza@gmail.com>
2026-07-16 22:51:07 -0400

    Implementado bosque procedural y atmósfera brutalista

--- my-digital-forest/components/canvas/Forest.js
+++ my-digital-forest/components/canvas/Forest.js
@@ -1,24 +1,28 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
-import { useState } from "react";
-// Comentamos la importación por un momento para aislar el error
-// import ForestGrid from "./ForestGrid"; 
+import { OrbitControls, PerspectiveCamera, Environment } from "@react-three/drei";
+import ForestGrid from "./ForestGrid";
 
 export default function Forest() {
   return (
-    <div style={{ width: '100vw', height: '100vh', position: 'relative' }}>
-      <Canvas camera={{ position: [25, 15, 25], fov: 30 }}>
-          <ambientLight intensity={0.5} />
-         <directionalLight position={[10, 10, 5]} intensity={1} castShadow />
-       <mesh castShadow receiveShadow>
-         {/* Ajustamos la geometría: ancho 2, altura 10, profundidad 2 */}
-            <boxGeometry args={[2, 10, 2]} /> 
-           {/* Material estilo concreto brutalista */}
-          <meshStandardMaterial color="#4a4a4a" roughness={0.8} />
-       </mesh>
-        <OrbitControls />
+    <div style={{ width: '100vw', height: '100vh', background: '#111111' }}>
+      <Canvas shadows>
+        <PerspectiveCamera makeDefault position={[30, 20, 30]} fov={45} />
+        <OrbitControls enablePan={true} enableZoom={true} />
+        
+        {/* Iluminación estilo cinematográfico brutalista */}
+        <ambientLight intensity={0.2} />
+        <directionalLight 
+          position={[10, 20, 10]} 
+          intensity={1} 
+          castShadow 
+          shadow-mapSize={[1024, 1024]}
+        />
+        <fog attach="fog" args={['#111111', 10, 60]} />
+
+        {/* El Bosque que acabamos de programar */}
+        <ForestGrid />
       </Canvas>
     </div>
   );
--- my-digital-forest/components/canvas/ForestGrid.js
+++ my-digital-forest/components/canvas/ForestGrid.js
@@ -1,8 +1,25 @@
 "use client";
 
+import { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
 
 export default function ForestGrid({ analyser }) {
-  // Por ahora, solo devolvemos un grupo vacío para descartar errores
-  return <group />;
+  const group = useRef();
+
+  // Generamos un bosque de 20 columnas brutalistas
+  const columns = Array.from({ length: 20 }, (_, i) => ({
+    position: [(Math.random() - 0.5) * 40, 5, (Math.random() - 0.5) * 40],
+    height: 5 + Math.random() * 10,
+  }));
+
+  return (
+    <group ref={group}>
+      {columns.map((col, index) => (
+        <mesh key={index} position={col.position} castShadow receiveShadow>
+          <boxGeometry args={[2, col.height, 2]} />
+          <meshStandardMaterial color="#333333" roughness={0.9} />
+        </mesh>
+      ))}
+    </group>
+  );
 }
